abd6bb556f3075ad16a65217b3f94d6091d38e2b
Yushan Cheng <b10432020@gapps.ntust.edu.tw>
2020-07-12 23:36:20 +0800

    complete model project test

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,19 +1,51 @@
 require 'rails_helper'
 
 RSpec.describe Project, type: :model do
-  it "does not allow duplicate project names per user" do
-    user = User.create(
+  before do
+    @user = User.create(
       first_name: "Joe",
       last_name: "Tester",
       email: "joetester@example.com",
       password: "dottle-nouveau-pavilion-tights-furze",
     )
+  end
 
-    user.projects.create(
+  context "with name and user" do
+    let(:project){ Project.new(owner: @user, name: "Test Project") }
+
+    it "should be vaild" do
+      expect(project).to be_valid
+    end
+  end
+
+  context "project without a name" do
+    before do
+      @project = Project.new(name: nil)
+      @project.valid?
+    end
+    
+    it "is invalid" do
+      expect(@project.errors[:name]).to include("can't be blank")
+    end
+  end
+
+  context "project without an owner" do
+    before do
+      @project = Project.new(owner: nil)
+      @project.valid?
+    end
+    it "is invalid" do
+      expect(@project.errors[:owner]).to include("must exist")
+    end
+  end
+
+  it "does not allow duplicate project names per user" do
+    
+    @user.projects.create(
       name: "Test Project"
     )
 
-    new_project = user.projects.build(
+    new_project = @user.projects.build(
       name: "Test Project",
     )
 
@@ -21,15 +53,8 @@ RSpec.describe Project, type: :model do
     expect(new_project.errors[:name]).to include("has already been taken")
   end
 
-  it "allows two users to share a project name" do #有 do...end 就不是 pending??
-    user = User.create(
-      first_name: "Joe",
-      last_name: "Tester",
-      email: "joetester@example.com",
-      password: "dottle-nouveau-pavilion-tights-furze",
-    )
-
-    user.projects.create(
+  it "allows two users to share a project name" do
+    @user.projects.create(
       name: "Test Project",
     )
 
